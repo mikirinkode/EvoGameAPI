ff02d1ae848b6e3b1947b452c717c4d7a98a961f
Muhammad Wafa <muhammadwafa105@gmail.com>
2022-10-24 15:19:42 +0700

    initial commit

---
+++ src/main/java/com/mikirinkode/evogame/EvogameApplication.java
@@ -0,0 +1,13 @@
+package com.mikirinkode.evogame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvogameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvogameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikirinkode/evogame/EvogameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikirinkode.evogame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EvogameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
